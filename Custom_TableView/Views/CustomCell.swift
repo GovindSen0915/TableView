2df19aac64d402b8be4490fd4b676b39959f16a3
Govind Sen <govindsen0915@gmail.com>
2023-09-02 11:53:55 +0530

    chore(): add custom cell file for tableview

---
+++ Custom_TableView/Views/CustomCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomCell.swift
+//  Custom_TableView
+//
+//  Created by Govind Sen on 02/09/23.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
