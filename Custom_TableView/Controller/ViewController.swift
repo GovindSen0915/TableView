a142ba1db22183eb7e5b0e8110e34bede0a6a790
Govind Sen <govindsen0915@gmail.com>
2023-09-02 11:43:12 +0530

    feat(): add tableview delegate datasource methods

--- Custom_TableView/Controller/ViewController.swift
+++ Custom_TableView/Controller/ViewController.swift
@@ -8,25 +8,51 @@
 import UIKit
 
 protocol ViewControllerProtocol: AnyObject {
-    
+    var numberOfSections: Int { get }
+    func numberOfItemsAt(section: Int) -> Int
+    func itemAt(indexPath: IndexPath) -> Any
 }
 
 class ViewController: UIViewController {
     
-    var viewModel: ViewControllerProtocol?
+    @IBOutlet weak var tableView: UITableView!
+    
+    var viewModel: ViewControllerProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupViewModel()
+        self.setupTableview()
     }
     
     private func setupViewModel() {
         self.viewModel = ViewModel(view: self)
     }
-
-
 }
 
 extension ViewController: ViewModelProtocol {
+    
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func setupTableview() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.viewModel.numberOfSections
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel.numberOfItemsAt(section: section)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
     
 }
 
